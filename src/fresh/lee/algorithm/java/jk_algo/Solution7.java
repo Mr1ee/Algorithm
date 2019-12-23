d08bce34c79f161875e2ca51f90faf036e0f0d9a
huayongli <huayongli@creditease.cn>
2019-12-23 11:11:46 +0800

    Double test

--- src/fresh/lee/algorithm/java/jk_algo/Solution7.java
+++ src/fresh/lee/algorithm/java/jk_algo/Solution7.java
@@ -21,11 +21,37 @@ public class Solution7 {
         return (int)result;
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         System.out.println(12345);
         System.out.println(new Solution7().reverse(12345));
         System.out.println(new Solution7().reverse(-12345));
         System.out.println(new Solution7().reverse(Integer.MIN_VALUE));
         System.out.println(new Solution7().reverse(Integer.MAX_VALUE));
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Double.parseDouble("1"));
+
+        // java.lang.NumberFormatException: empty String
+        //System.out.println(Double.parseDouble(""));
+
+        // NullPointerException
+        //System.out.println(Double.parseDouble(null));
+        int i = 2;
+        switch (i) {
+            case 4:
+                System.out.println(4);
+            case 3:
+                System.out.println(3);
+            case 2:
+                System.out.println(2);
+            case 1:
+                System.out.println(1);
+            case 0:
+                System.out.println(0);
+            default:
+                System.out.println("default");
+        }
     }
 }
